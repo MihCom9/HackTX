d5a837ecd97e5bf9900631a706e98364b5eebfdf
Aleksandar Beshev <161361662+MrBeshev@users.noreply.github.com>
2024-03-14 11:23:28 +0200

    Add files via upload
    Код за следене на количеството вода в контейнера

---
+++ waterContainer.ino
@@ -0,0 +1,42 @@
+#define sensorPower 7
+#define sensorPin A0
+int level = 0l;
+int quant = 0;
+bool check = false;
+//промени стойностите на променливите ако трябва
+
+int val = 0;
+
+void setup() {
+
+  pinMode(sensorPower, OUTPUT);
+
+  digitalWrite(sensorPower, LOW);
+}
+
+void loop() {
+
+  level = readSensor();
+  quant = level * 15 * 15;
+  // код за изпращане на количеството вода към сайта
+  delay(1000);
+}
+
+
+int readSensor() {
+  digitalWrite(sensorPower, HIGH);
+  delay(10);
+  val = analogRead(sensorPin);
+  digitalWrite(sensorPower, LOW);
+  return val;
+}
+
+void waterCap() {
+  if (quant < 100 && check == false) {
+  //код за изпращане на съобщение до сайта: "Остава малко вода (под 100)"
+  check = true;
+  }
+  if (quant > 150) {
+  check = false;
+  }
+}
